12ddd69e715d1fa5d5a18c485294b306991bb6fa
Matteo Zucchetti <matteo.zucchetti@playground.it>
2020-04-21 16:43:20 +0200

    test dark mode

--- src/routes/home.js
+++ src/routes/home.js
@@ -95,6 +95,7 @@ export default class Home extends Component {
             rel="noopener noreferrer"
             class="mb-5 text-xs block text-gray-500 hover:underline"
           >Se vuoi crearlo per la tua città visita la pagina GitHub del progetto</a>
+          sistemare --> <a href="https://www.iubenda.com/privacy-policy/66156653" class="iubenda-black iubenda-embed" title="Privacy Policy ">Privacy Policy</a>
 				</div>
 			</Fragment>
 		);
