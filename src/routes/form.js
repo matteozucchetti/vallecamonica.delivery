5d7ce27d7ec7c92eb7e4a185a9f0bd6062c6a432
Matteo Zucchetti <matteo.zucchetti@playground.it>
2020-04-21 16:30:27 +0200

    test form notification

--- src/routes/form.js
+++ src/routes/form.js
@@ -1,9 +1,9 @@
 export default function Form() {
 	return (
 		<div>
-			<form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/form/success">
+			<form name="VallecamonicaDeliveryForm" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/form/success">
 				<p>
-					<input type="hidden" name="form-name" value="contact" />
+					<input type="hidden" name="form-name" value="VallecamonicaDeliveryForm" />
 				</p>
 				<p class="my-5">
 					<label class="lock text-gray-800 ml-2 font-bold md:text-right mb-1 md:mb-0 pr-4">Nome
@@ -17,7 +17,7 @@ export default function Form() {
 				</p>
 				<p class="my-5">
 					<label class="lock text-gray-800 ml-2 font-bold md:text-right mb-1 md:mb-0 pr-4">Email
-						<input class="bg-white focus:outline-none focus:shadow-outline border border-gray-500 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="text" name="mail" />
+						<input class="bg-white focus:outline-none focus:shadow-outline border border-gray-500 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" required type="text" name="mail" />
 					</label>
 				</p>
 				<p class="my-5">
@@ -25,19 +25,11 @@ export default function Form() {
 						<input class="bg-white focus:outline-none focus:shadow-outline border border-gray-500 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="text" name="site" />
 					</label>
 				</p>
-				<p class="my-5">
-					<label class="block text-gray-800 ml-2 font-bold md:text-right mb-1 md:mb-0 pr-4">Consegna in tutta la provincia</label>
-					<div class="flex">
-						<label class="flex flex-col mr-5 px-2">
-							<span>Si</span>
-							<input type="radio" name="province-delivery" required value="yes" />
-						</label>
-						<label class="flex flex-col mr-5 px-2">
-							<span>No</span>
-							<input type="radio" name="province-delivery" required value="no" />
-						</label>
-					</div>
-				</p>
+        <p class="my-5">
+          <label class="lock text-gray-800 ml-2 font-bold md:text-right mb-1 md:mb-0 pr-4">Dove consegni? (specifica qui i comuni in cui puoi effettuare le consegne)
+            <textarea class="bg-white focus:outline-none focus:shadow-outline border border-gray-500 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" required type="text" name="delivery city" />
+          </label>
+        </p>
 				<p class="my-5">
 					<label class="lock text-gray-800 ml-2 font-bold md:text-right mb-1 md:mb-0 pr-4">Note
 						<textarea class="bg-white focus:outline-none focus:shadow-outline border border-gray-500 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="text" name="note" />
