1f2c9bd61d997662f26dba499096a56b61870de5
Matteo Zucchetti <matteo.zucchetti@playground.it>
2020-04-27 10:09:39 +0200

    robots

--- src/components/footer.js
+++ src/components/footer.js
@@ -27,11 +27,20 @@ export default class Footer extends Component {
             target="_blank"
             rel="noopener noreferrer"
             class="mb-5 text-xs block text-gray-500 hover:underline"
-          >Se vuoi crearlo per la tua città visita la pagina GitHub del progetto</a>
-          <p class="text-center">
+          >Se vuoi creare una piattaforma simile per la tua città, visita la pagina GitHub del progetto</a>
+          <p class="mb-5 text-center">
             <a href="https://www.iubenda.com/privacy-policy/19385130" class="iubenda-white iubenda-embed" title="Privacy Policy ">Privacy Policy</a>
             <a href="https://www.iubenda.com/privacy-policy/19385130/cookie-policy" class="iubenda-white iubenda-embed" title="Cookie Policy ">Cookie Policy</a>
           </p>
+          {/*<p class="text-center pb-2 text-xs block text-gray-500">
+            Se vuoi supportarci, fai una donazione
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+              <input type="hidden" name="cmd" value="_s-xclick" />
+              <input type="hidden" name="hosted_button_id" value="QP2S2PSUTXZE6" />
+              <input type="image" src="https://placehold.it/100x30" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Fai una donazione con il pulsante PayPal" />
+              <img alt="" border="0" src="https://www.paypal.com/it_IT/i/scr/pixel.gif" width="1" height="1" />
+            </form>
+          </p>*/}
         </div>      
       </Fragment>
     );
