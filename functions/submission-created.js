ddd49f73baadb2c50e5fdd63409e6730975fe40b
Matteo Zucchetti <matteo.zucchetti@playground.it>
2020-05-03 13:36:54 +0200

    cane

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,5 +1,6 @@
 exports.handler = function(event, context, callback) {
   const parsedBody = JSON.parse(event.body);
+  console.log(parsedBody)
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(parsedBody)
