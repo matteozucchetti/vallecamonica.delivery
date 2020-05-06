512ff66aaff1e1d5a02f233e45a723211fa8a2de
Matteo Zucchetti <matteo.zucchetti@playground.it>
2020-05-06 20:23:16 +0200

    test

--- src/routes/form.js
+++ src/routes/form.js
@@ -19,10 +19,10 @@ export default function Form() {
           <span class="bg-white inline-block relative z-10 px-10 uppercase">AGGIUNGI LA TUA ATTIVITÀ</span>
         </h2>
         <p class="mb-5 text-center"><b>Compila il form</b> qui sotto per <b>inviare la richiesta</b> e aggiungere la tua attività</p>
-  			<form class="mt-10" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/form/success">
-  				<p>
-  					<input type="hidden" name="form-name" value="contact" />
-  				</p>
+  			<form class="mt-10" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="">
+
+  				<input type="hidden" aria-hidden="true" name="form-name" value="contact" />
+          <input type="hidden" aria-hidden="true" name="bot-field" />
 
 
           <div class="flex flex-wrap">
