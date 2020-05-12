80ba5d0529bf690ecd1f36fc8b25b72db93046f6
Matteo Zucchetti <matteo.zucchetti@playground.it>
2020-05-12 20:26:09 +0200

    icone fb e insta

--- src/components/dialog.js
+++ src/components/dialog.js
@@ -12,65 +12,67 @@ import { D_MinOrder } from './dialog_min_order.js';
 import CloseIcon from '../assets/svg/icon_close.svg';
 
 export const Dialog = ({
-	isOpen,
-	closePopup,
-  closePopupFromButton,
-	name,
-	tel,
-	mail,
-	site,
-	services,
-	payments,
-  where,
-  when,
-	note,
-  min_order,
-  delivery_fee
+   isOpen,
+   closePopup,
+   closePopupFromButton,
+   name,
+   tel,
+   mail,
+   site,
+   services,
+   payments,
+   where,
+   when,
+   note,
+   min_order,
+   delivery_fee,
+   insta,
+   facebook
 }) => {
-	return (
-		<dialog 
-			class="fixed inset-x-0 top-0 backdrop w-screen h-screen z-20 vcdDialog overflow-y-scroll overflow-x-hidden"
-			style={{display: isOpen ? "initial" : "none"}}
-      onClick={closePopup}
-      id="popupDialog"
-		>
-			<div
-				class="w-full md:w-5/6 max-w-screen-lg shadow-lg bg-white vcdDialog-inner mb-10 pb-10 border-b-8 border-vcd-rosa"
-			>
-				<div class="flex justify-start md:justify-center items-center bg-vcd-azzurro p-3 md:p-6 relative">
+   return (
+      <dialog
+         class="fixed inset-x-0 top-0 backdrop w-screen h-screen z-20 vcdDialog overflow-y-scroll overflow-x-hidden"
+         style={{ display: isOpen ? "initial" : "none" }}
+         onClick={closePopup}
+         id="popupDialog"
+      >
+         <div
+            class="w-full md:w-5/6 max-w-screen-lg shadow-lg bg-white vcdDialog-inner mb-10 pb-10 border-b-8 border-vcd-rosa"
+         >
+            <div class="flex justify-start md:justify-center items-center bg-vcd-azzurro p-3 md:p-6 relative">
 
-					<h2 class="text-2xl font-bold text-white pr-10 md:pr-0 leading-tight">{name}</h2>
+               <h2 class="text-2xl font-bold text-white pr-10 md:pr-0 leading-tight">{name}</h2>
 
-					<button
-						class="vcd-closeButton z-30"
-            onClick={closePopupFromButton}
-					><CloseIcon /></button>
+               <button
+                  class="vcd-closeButton z-30"
+                  onClick={closePopupFromButton}
+               ><CloseIcon /></button>
 
-				</div>
-
-        <div class="px-3 md:px-6 relative">
-
-          {where && <D_Where {...{where}} />}
-          {when && <D_When {...{when}} />}
-          {
-            (delivery_fee || min_order)
-            ?
-            <div class="vcd-dialogBox justify-center items-center">
-              {delivery_fee && <div class="px-5"><D_DeliveryFee {...{delivery_fee}} /></div>}
-              {min_order && <div class="px-5"><D_MinOrder {...{min_order}} /></div>}
             </div>
-            : null
-          }
-          {note && <D_Notes {...{note}} />}
-          <D_Contacts {...{tel, mail, site, name}} />
 
-          {/*
+            <div class="px-3 md:px-6 relative">
+
+               {where && <D_Where {...{ where }} />}
+               {when && <D_When {...{ when }} />}
+               {
+                  (delivery_fee || min_order)
+                     ?
+                     <div class="vcd-dialogBox justify-center items-center">
+                        {delivery_fee && <div class="px-5"><D_DeliveryFee {...{ delivery_fee }} /></div>}
+                        {min_order && <div class="px-5"><D_MinOrder {...{ min_order }} /></div>}
+                     </div>
+                     : null
+               }
+               {note && <D_Notes {...{ note }} />}
+               <D_Contacts {...{ tel, mail, site, name, insta, facebook }} />
+
+               {/*
             {payments && <D_Payments {...{payments}} />}
             {services && <D_Services {...{services}} />}
           */}
 
-			 </div>
-      </div>
-		</dialog>
-	);
+            </div>
+         </div>
+      </dialog>
+   );
 };
--- src/components/dialog_contacts.js
+++ src/components/dialog_contacts.js
@@ -9,69 +9,94 @@ import gtagEvent from '../utils/gtagEvents.js';
 // Images
 import MailIcon from '../assets/svg/icon_mail.svg';
 import WebsiteIcon from '../assets/svg/icon_website.svg';
+import InstaIcon from '../assets/svg/icon_insta.svg';
 import FacebookIcon from '../assets/svg/icon_facebook.svg';
 import TelIcon from '../assets/svg/icon_tel.svg';
 
-function Tel({tel, name}) {
+function Tel({ tel, name }) {
 	const isArrayOfTel = Array.isArray(tel);
 
-	return ( isArrayOfTel
+	return (isArrayOfTel
 		? tel.map((telNumber, i) => (
-			<a href={`tel:${telNumber}`} onClick={() => {gtagEvent('custom_click','dialog - '+name,'tel number')}} class="inline-block rounded-lg text-sm">
+			<a href={`tel:${telNumber}`} onClick={() => { gtagEvent('custom_click', 'dialog - ' + name, 'tel number') }} class="inline-block rounded-lg text-sm">
 				<span>{telNumber}</span>
-				{tel.length !== i+1 && <span class="mx-2">-</span>}
+				{tel.length !== i + 1 && <span class="mx-2">-</span>}
 			</a>
 		))
-		: <a href={`tel:${tel}`} onClick={() => {gtagEvent('custom_click','dialog - '+name,'tel number')}} class="inline-block rounded-lg text-sm">
+		: <a href={`tel:${tel}`} onClick={() => { gtagEvent('custom_click', 'dialog - ' + name, 'tel number') }} class="inline-block rounded-lg text-sm">
 			{tel}
 		</a>
 	);
 }
 
-export const D_Contacts = ({tel, mail, site, name}) => {
+export const D_Contacts = ({ tel, mail, site, name, insta, facebook }) => {
 	return (
 		<Fragment>
-      <div class="vcd-dialogBox border-none">
-        <div class="w-full">
-    			<h3 class="mt-4 mb-2">Contatti</h3>
-    			<div class="mb-2 mt-4 text-sm flex flex-wrap justify-center items-start">
-    				{tel && (
-    					<div class="text-center flex-auto md:flex-1 mb-2">
-                <TelIcon
-                  class="w-4 h-4 mx-auto mb-2 contact-icon"
-                  role="img"
-                  aria-label="telephone"
-                />
-    						<Tel {...{tel, name}} />
-    					</div>
-    				)}
-    				{site && (
-    					<div class="text-center flex-auto md:flex-1 mb-2 px-2 md:px-0">
-                <a href={site} onClick={() => {gtagEvent('custom_click','dialog - website',name)}} target="_blank">
-      						<WebsiteIcon
-                    class="w-4 h-4 mx-auto mb-2 contact-icon"
-                    role="img"
-                    aria-label="see the site"
-                  />    						
-    							<span>{cleanUrls(site)}</span>
-    						</a>
-    					</div>
-    				)}
-    				{mail && (
-    					<div class="text-center flex-auto md:flex-1 mb-2 px-2 md:px-0">
-                <a href={`mailto:${mail}`} onClick={() => {gtagEvent('custom_click','dialog - email',name)}}>
-      						<MailIcon
-                    class="w-4 h-4 mx-auto mb-2 contact-icon"
-                    role="img"
-                    aria-label="email"
-                  />
-    							<span>{mail}</span>
-    						</a>
-    					</div>
-    				)}
-    			</div>
-        </div>
-      </div>
+			<div class="vcd-dialogBox border-none">
+				<div class="w-full">
+					<h3 class="mt-4 mb-2">Contatti</h3>
+					<div class="mb-2 mt-4 text-sm flex flex-wrap justify-center items-start">
+						{tel && (
+							<div class="text-center flex-auto md:flex-1 mb-2">
+								<TelIcon
+									class="w-4 h-4 mx-auto mb-2 contact-icon"
+									role="img"
+									aria-label="telephone"
+								/>
+								<Tel {...{ tel, name }} />
+							</div>
+						)}
+						{site && (
+							<div class="text-center flex-auto md:flex-1 mb-2 px-2 md:px-0">
+								<a href={site} onClick={() => { gtagEvent('custom_click', 'dialog - website', name) }} target="_blank">
+									<WebsiteIcon
+										class="w-4 h-4 mx-auto mb-2 contact-icon"
+										role="img"
+										aria-label="see the site"
+									/>
+									<span>{cleanUrls(site)}</span>
+								</a>
+							</div>
+						)}
+						{mail && (
+							<div class="text-center flex-auto md:flex-1 mb-2 px-2 md:px-0">
+								<a href={`mailto:${mail}`} onClick={() => { gtagEvent('custom_click', 'dialog - email', name) }}>
+									<MailIcon
+										class="w-4 h-4 mx-auto mb-2 contact-icon"
+										role="img"
+										aria-label="email"
+									/>
+									<span>{mail}</span>
+								</a>
+							</div>
+						)}
+						{insta && (
+							<div class="text-center w-full md:w-auto flex-auto md:flex-1 mb-2 px-2">
+								<a href={insta} target="_blank" onClick={() => { gtagEvent('custom_click', 'dialog - insta', name) }}>
+									<InstaIcon
+										class="w-4 h-4 mx-auto mb-2 contact-icon"
+										role="img"
+										aria-label="instagram"
+									/>
+									<span>{name}</span>
+								</a>
+							</div>
+						)}
+						{facebook && (
+							<div class="text-center w-full md:w-auto flex-auto md:flex-1 mb-2 px-2">
+								<a href={facebook} target="_blank" onClick={() => { gtagEvent('custom_click', 'dialog - facebook', name) }}>
+									<FacebookIcon
+										class="w-4 h-4 mx-auto mb-2 contact-icon"
+										role="img"
+										aria-label="instagram"
+									/>
+									<span>{name}</span>
+								</a>
+							</div>
+						)}
+					</div>
+				</div>
+			</div>
 		</Fragment>
 	);
 }
